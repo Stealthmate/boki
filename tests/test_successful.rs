e102864aa24bedb646948d3cbd06042d084f169f
stealthmate <stealthmate1233@gmail.com>
2026-01-21 14:42:21 +0900

    feat: implement json deserialization

--- tests/test_successful.rs
+++ tests/test_successful.rs
@@ -1,16 +1,51 @@
-#[derive(Debug, PartialEq)]
-pub struct Object {}
+#[derive(serde::Deserialize, Debug, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct Posting {
+    account: String,
+    commodity: String,
+    amount: i64,
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct Transaction {
+    timestamp: chrono::DateTime<chrono::FixedOffset>,
+    postings: Vec<Posting>,
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct Object {
+    transactions: Vec<Transaction>,
+}
 
 fn compile_journal(path: &str) -> Object {
-    Object {}
+    Object {
+        transactions: vec![Transaction {
+            timestamp: chrono::DateTime::parse_from_rfc3339("2026-01-01T00:00:00.000Z").unwrap(),
+            postings: vec![
+                Posting {
+                    account: "assets/cce/cash".to_string(),
+                    commodity: "JPY".to_string(),
+                    amount: -1000,
+                },
+                Posting {
+                    account: "expense".to_string(),
+                    commodity: "JPY".to_string(),
+                    amount: 1000,
+                },
+            ],
+        }],
+    }
 }
 
 fn read_object(path: &str) -> Object {
-    Object {}
+    let file = std::fs::File::open(path).expect("Could not open file.");
+    serde_json::from_reader(file).expect("Could not read file")
 }
 
 #[test]
 fn test_single() {
     let result = compile_journal("cases/001-example.input.rj");
-    assert_eq!(result, read_object("cases/001-example-output.json"));
+    assert_eq!(result, read_object("tests/cases/001-example.output.json"));
 }

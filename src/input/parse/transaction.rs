28c9caa3e7c7f6ba9cf7d98e202dd0a8bb2bd0e2
stealthmate <stealthmate1233@gmail.com>
2026-01-29 08:56:15 +0900

    feat: simple transaction parse test

--- src/input/parse/transaction.rs
+++ src/input/parse/transaction.rs
@@ -18,3 +18,40 @@ impl TransactionParser {
         ))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::TransactionParser;
+    use crate::input::parse::syntax::{Timestamp, Token};
+
+    fn sample_timestamp() -> Timestamp {
+        Timestamp::parse_from_rfc3339("2026-01-01T00:00:00.000+09:00").unwrap()
+    }
+
+    #[test]
+    fn test_simple() {
+        let ts = sample_timestamp();
+        let tokens = [
+            Token::Timestamp(ts),
+            Token::LineSeparator,
+            Token::Indent,
+            Token::Identifier("asset".to_string()),
+            Token::AccountSeparator,
+            Token::Identifier("cce".to_string()),
+            Token::AccountSeparator,
+            Token::Identifier("cash".to_string()),
+            Token::PostingSeparator,
+            Token::Identifier("JPY".to_string()),
+            Token::PostingSeparator,
+            Token::Amount(1000),
+            Token::LineSeparator,
+            Token::Identifier("expense".to_string()),
+            Token::PostingSeparator,
+            Token::Identifier("JPY".to_string()),
+            Token::PostingSeparator,
+            Token::Amount(1000),
+        ];
+        let (rest, result) = TransactionParser::parse(&tokens).expect("Failed.");
+        assert!(rest.is_empty());
+    }
+}

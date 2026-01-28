b06a2e93efa1d38c6ca18ae0d9f66aeee94d7077
stealthmate <stealthmate1233@gmail.com>
2026-01-28 20:32:11 +0900

    feat: compile literal transactions

--- src/input/compile.rs
+++ src/input/compile.rs
@@ -8,10 +8,17 @@ pub fn compile_transaction(
 ) -> Result<(), String> {
     journal.transactions.push(output::Transaction {
         header: output::TransactionHeader {
-            timestamp: chrono::DateTime::parse_from_rfc3339("2026-01-01T00:00:00.000+09:00")
-                .unwrap(),
+            timestamp: t.header.timestamp,
         },
-        postings: vec![],
+        postings: t
+            .postings
+            .iter()
+            .map(|p| output::Posting {
+                account: p.account.clone(),
+                commodity: p.commodity.clone().unwrap(),
+                amount: p.amount.unwrap(),
+            })
+            .collect(),
     });
 
     Ok(())
@@ -69,5 +76,35 @@ mod test {
         let result = compile_node(&node, &mut journal).expect("Compilation failed.");
 
         assert_eq!(journal.transactions.len(), 1);
+    }
+
+    #[test]
+    fn test_compile_transaction_all_literals() {
+        let t = ast::Transaction {
+            header: ast::TransactionHeader {
+                timestamp: chrono::DateTime::parse_from_rfc3339("2026-01-02T03:04:05.000+09:00")
+                    .unwrap(),
+            },
+            postings: vec![ast::Posting {
+                account: "foo".to_string(),
+                commodity: Some("JPY".to_string()),
+                amount: Some(1000),
+            }],
+        };
+
+        let mut journal = output::Journal {
+            transactions: vec![],
+        };
+
+        let result = compile_transaction(&t, &mut journal);
+
+        let j_t = journal.transactions.first().expect("Failed.");
+        assert_eq!(j_t.header.timestamp, t.header.timestamp);
+        assert_eq!(j_t.postings.len(), t.postings.len());
+        for (p_out, p_in) in j_t.postings.iter().zip(t.postings.iter()) {
+            assert_eq!(p_out.account, p_in.account);
+            assert_eq!(p_out.commodity, p_in.commodity.clone().unwrap());
+            assert_eq!(p_out.amount, p_in.amount.clone().unwrap());
+        }
     }
 }

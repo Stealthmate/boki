753d469c892072cbf78d4519a87bcdde825e291e
stealthmate <stealthmate1233@gmail.com>
2026-01-29 08:35:45 +0900

    feat: transaction parse test

--- src/input/parse.rs
+++ src/input/parse.rs
@@ -11,6 +11,7 @@ pub enum Token {
     Identifier(String),
     AccountSeparator,
     PostingSeparator,
+    LineSeparator,
     Comment(String),
     Indent,
     Dedent,
@@ -37,9 +38,31 @@ fn parse_comments(tokens: &[Token]) -> ParserResult<'_, ()> {
     Ok((rest, ()))
 }
 
+fn parse_node(tokens: &[Token]) -> ParserResult<'_, ast::ASTNode> {
+    Ok((
+        &[],
+        ast::ASTNode::Transaction(ast::Transaction {
+            header: ast::TransactionHeader {
+                timestamp: DateTime::parse_from_rfc3339("2026-01-01T00:00:00.000+09:00").unwrap(),
+            },
+            postings: vec![],
+        }),
+    ))
+}
+
 pub fn parse_tokens(tokens: &[Token]) -> ParserResult<'_, Vec<ast::ASTNode>> {
     let (tokens, _) = parse_comments(tokens)?;
-    Ok((tokens, vec![]))
+
+    let mut rest = tokens;
+    let mut nodes = vec![];
+
+    while !rest.is_empty() {
+        let (next_rest, node) = parse_node(rest)?;
+        rest = next_rest;
+        nodes.push(node);
+    }
+
+    Ok((rest, nodes))
 }
 
 #[cfg(test)]
@@ -61,6 +84,39 @@ mod test {
         ];
         let (rest, result) = parse_tokens(&tokens).expect("Failed.");
         assert!(result.is_empty());
+        assert!(rest.is_empty());
+    }
+
+    #[test]
+    fn test_transaction() {
+        let ts = DateTime::parse_from_rfc3339("2026-01-01T00:00:00.000+09:00").unwrap();
+        let tokens = [
+            Token::Timestamp(ts),
+            Token::LineSeparator,
+            Token::Indent,
+            Token::Identifier("asset".to_string()),
+            Token::AccountSeparator,
+            Token::Identifier("cce".to_string()),
+            Token::AccountSeparator,
+            Token::Identifier("cash".to_string()),
+            Token::PostingSeparator,
+            Token::Identifier("JPY".to_string()),
+            Token::PostingSeparator,
+            Token::Amount(1000),
+            Token::LineSeparator,
+            Token::Identifier("expense".to_string()),
+            Token::PostingSeparator,
+            Token::Identifier("JPY".to_string()),
+            Token::PostingSeparator,
+            Token::Amount(1000),
+        ];
+        let (rest, result) = parse_tokens(&tokens).expect("Failed.");
+        let node = result
+            .first()
+            .expect("Should have parsed at least one node.");
+        let ast::ASTNode::Transaction(t) = node else {
+            panic!("Should have parsed a transaction.");
+        };
         assert!(rest.is_empty());
     }
 }

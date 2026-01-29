3e09c3b5fd1454c428c963a9bfac3b61c14b9b86
stealthmate <stealthmate1233@gmail.com>
2026-01-29 19:30:56 +0900

    feat: identifier lexing

--- src/input/lex.rs
+++ src/input/lex.rs
@@ -1,8 +1,9 @@
 use crate::input::parse::Token;
-use nom::bytes::complete::is_a;
-use nom::combinator::opt;
+use nom::bytes::complete::{is_a, tag};
+use nom::character::complete::{alpha1, alphanumeric0, none_of};
+use nom::combinator::{opt, peek};
 use nom::multi::{many0, many1};
-use nom::sequence::{preceded, terminated};
+use nom::sequence::{pair, preceded, terminated};
 use nom::Parser;
 
 type LexResult<'a, T> = nom::IResult<&'a str, T, nom_language::error::VerboseError<&'a str>>;
@@ -22,14 +23,40 @@ fn lex_linespace(input: &str) -> LexResult<'_, ()> {
     Ok((input, ()))
 }
 
+fn lex_indent(input: &str) -> LexResult<'_, Token> {
+    let (input, _) = tag("  ").parse(input)?;
+    let (input, _) = peek(none_of("\n")).parse(input)?;
+    Ok((input, Token::Indent))
+}
+
+fn lex_identifier(input: &str) -> LexResult<'_, Token> {
+    let (input, x) = pair(alpha1, alphanumeric0).parse(input)?;
+    Ok((input, Token::Identifier(x.0.to_string() + x.1)))
+}
+
 fn lex_single_token(input: &str) -> LexResult<'_, Token> {
-    Ok(("", Token::Indent))
+    let mut results = vec![];
+    for mut lexer in [lex_identifier, lex_indent] {
+        if let Ok(x) = lexer.parse(input) {
+            results.push(x);
+        }
+    }
+
+    results.sort_by(|a, b| a.0.len().cmp(&b.0.len()));
+    let Some(result) = results.first().cloned() else {
+        return Err(nom::Err::Error(nom::error::make_error(
+            input,
+            nom::error::ErrorKind::IsNot,
+        )));
+    };
+
+    Ok(result)
 }
 
 pub fn lex_string(input: &str) -> LexResult<'_, Vec<Token>> {
     preceded(
         opt(lex_linespace),
-        many0(terminated(lex_single_token, lex_linespace)),
+        many0(terminated(lex_single_token, opt(lex_linespace))),
     )
     .parse(input)
 }
@@ -37,6 +64,7 @@ pub fn lex_string(input: &str) -> LexResult<'_, Vec<Token>> {
 #[cfg(test)]
 mod test {
     use crate::input::lex::lex_string;
+    use crate::input::parse::Token;
 
     #[test]
     fn test_empty() {
@@ -54,11 +82,23 @@ mod test {
         assert!(rest.is_empty());
     }
 
-    // #[test]
-    // fn test_single_token() {
-    //     let input = "example";
-    //     let (rest, tokens) = lex_string(input).expect("Failed.");
-    //     assert_eq!(tokens.len(), 1);
-    //     assert!(rest.is_empty());
-    // }
+    #[test]
+    fn test_1_token() {
+        let input = "foo";
+        let (rest, tokens) = lex_string(input).expect("Failed.");
+        let tok = tokens.first().expect("Should have lexed at least 1 token.");
+        let Token::Identifier(x) = tok else {
+            panic!("Should have been an identifier token.");
+        };
+        assert_eq!(x, "foo");
+        assert!(rest.is_empty());
+    }
+
+    #[test]
+    fn test_2_token_with_space_inbetween() {
+        let input = "\n    \nfoo  \n \n \t\t  \n\n\nbar\n\n";
+        let (rest, tokens) = lex_string(input).expect("Failed.");
+        assert_eq!(tokens.len(), 2);
+        assert!(rest.is_empty());
+    }
 }

f64503a70a65a715242cdbdbda94e61201f1e7d1
stealthmate <stealthmate1233@gmail.com>
2026-01-29 10:13:31 +0900

    feat: generic parsing utilities

--- src/input/parse/core.rs
+++ src/input/parse/core.rs
@@ -8,3 +8,36 @@ pub fn next(tokens: &[syntax::Token]) -> ParserResult<'_, syntax::Token> {
         Some(x) => Ok((&tokens[1..], x.clone())),
     }
 }
+
+pub trait Parser<'a, T> {
+    fn parse(&self, tokens: &'a [syntax::Token]) -> ParserResult<'a, T>;
+}
+
+impl<'a, T, F> Parser<'a, T> for F
+where
+    F: Fn(&'a [syntax::Token]) -> ParserResult<'a, T>,
+{
+    fn parse(&self, tokens: &'a [syntax::Token]) -> ParserResult<'a, T> {
+        self(tokens)
+    }
+}
+
+struct ManyParser<P> {
+    parser: P,
+}
+
+impl<'a, T, P> Parser<'a, Vec<T>> for ManyParser<P>
+where
+    P: Parser<'a, T>,
+{
+    fn parse(&self, tokens: &'a [syntax::Token]) -> ParserResult<'a, Vec<T>> {
+        todo!()
+    }
+}
+
+pub fn many<'a, P1, T>(parser: P1) -> impl Parser<'a, Vec<T>>
+where
+    P1: Parser<'a, T>,
+{
+    ManyParser { parser }
+}

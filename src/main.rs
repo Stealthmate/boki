9cd3aec165534c77ca44ff0939938c2ec05ac3a0
stealthmate <stealthmate1233@gmail.com>
2026-02-04 19:57:59 +0900

    feat: minimal CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Export {
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+    },
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    file: PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
 fn main() {
-    println!("Hello!");
+    let cli = Cli::parse();
+
+    let source = std::fs::read_to_string(cli.file).expect("Failed to read journal.");
+    let journal = boki::compile_string(&source).expect("Failed to compile journal.");
+
+    match &cli.command {
+        Commands::Export { output } => {
+            let output_str = serde_json::to_string(&journal).expect("Failed to serialize journal.");
+            match output {
+                None => println!("{output_str}"),
+                Some(x) => std::fs::write(x, output_str).expect("Failed to write output file."),
+            };
+        }
+    }
 }

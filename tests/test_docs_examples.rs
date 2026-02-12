71e1e8b1fec4513faa0ee604a58e5681066dcc46
stealthmate <stealthmate1233@gmail.com>
2026-02-12 20:25:48 +0900

    test: simplify integration tests
    I removed the old cases and replaced them with a single test that
    checks the example provided in docs. That accomplishes the same thing
    and is simpler for now.

---
+++ tests/test_docs_examples.rs
@@ -0,0 +1,7 @@
+use boki;
+
+#[rstest::rstest]
+#[case::books_journal("docs/examples/01-books-journal/books.boki")]
+fn test_docs(#[case] case: &str) {
+    boki::evaluate::evaluate_file(case).expect("Failed.");
+}

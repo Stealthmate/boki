6511589b61ac165baadca2ba4bb8af3541b8a7d0
stealthmate <stealthmate1233@gmail.com>
2026-02-18 09:20:32 +0900

    docs: root module documentation
    Some preparatory work on #25

--- justfile
+++ justfile
@@ -2,5 +2,17 @@ format:
     cargo fmt
     cargo clippy
 
+dep-graph:
+    cargo modules dependencies \
+        --no-sysroot \
+        --no-fns \
+        --no-types \
+        --no-private \
+        --no-traits \
+        --layout dot \
+        --lib \
+        | sed 's/constraint=false/constraint=true/g' \
+        | dot -Tpng > .ignored/mods.png
+
 test:
     cargo test -- --test-threads=1
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! # Boki
+//!
+//! boki is a tool to help humans working with accounting data, a la [plain text accounting](https://plaintextaccounting.org/).
+//!
+//! ## Module structure
+//!
+//! 1. Only root modules can be public.
+//! 2. Public modules must never form a dependency cycle.
+//! 3. Public modules must be used directly either by the binary, or by two or more other public modules.
+
 pub mod compile;
 pub mod contracts;
 pub mod evaluate;

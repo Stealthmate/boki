576d900339d1c97484588c3e02581e49825e961e
stealthmate <stealthmate1233@gmail.com>
2026-03-02 13:13:02 +0900

    fix: do not eat whitespace in identifier lex
    ref: #31

--- src/lex/identifier.rs
+++ src/lex/identifier.rs
@@ -1,9 +1,8 @@
 use super::core::{NomResult, StringScanner};
-use super::whitespace;
 use crate::tokens::Token;
 use nom::bytes::complete::is_a;
 use nom::combinator::{opt, recognize};
-use nom::sequence::{pair, preceded};
+use nom::sequence::pair;
 use nom::Parser;
 
 const ALPHA_UNDERSCORE: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJQKLMNOPQRSTUVWXYZ_";
@@ -11,13 +10,10 @@ const ALPHA_UNDERSCORE_DIGIT: &str =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJQKLMNOPQRSTUVWXYZ_0123456789";
 
 pub fn lex(input: StringScanner) -> NomResult<Token> {
-    let (input, x) = preceded(
-        opt(whitespace::whitespace),
-        recognize(pair(
-            is_a(ALPHA_UNDERSCORE),
-            opt(is_a(ALPHA_UNDERSCORE_DIGIT)),
-        )),
-    )
+    let (input, x) = recognize(pair(
+        is_a(ALPHA_UNDERSCORE),
+        opt(is_a(ALPHA_UNDERSCORE_DIGIT)),
+    ))
     .parse(input)?;
 
     Ok((input, Token::Identifier(x.as_str().to_string())))
@@ -35,15 +31,6 @@ mod test {
             panic!("Should have been an identifier.");
         };
         assert_eq!(x, input);
-    }
-
-    #[test]
-    fn test_identifier_leading_whitespace() {
-        let (_, output) = super::lex("   foo".into()).expect("Failed.");
-        let super::Token::Identifier(x) = output else {
-            panic!("Should have been an identifier.");
-        };
-        assert_eq!(x, "foo");
     }
 
     #[rstest::rstest]
